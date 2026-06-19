2cc712c82c40880857a5364388a81432a4caf931
jasmine <jasminemohamed174005@gmail.com>
2026-06-19 13:32:42 +0300

    implement Result Pattern

---
+++ E_commerce.Application/Common/Error.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace E_commerce.Application.Common
+{
+    public record Error(string Code , string Description , ErrorType ErrorType = ErrorType.Failure )
+    {
+
+        public static Error Failure(string code = "General.Failure", string description = "General.Failure Error has occured") => new Error(code, description, ErrorType.Failure);
+        public static Error Validation(string code = "General.Validation", string description = "General.Validation Error has occured") => new(code , description, ErrorType.Validation);
+        public static Error NotFound(string code="General.NotFound", string description = "General.Not Found Error has occured") => new Error(code , description, ErrorType.NotFound);
+        public static Error Conflict(string code = "General.Conflict", string description = "General.Conflict Error F has occured") => new Error(code, description, ErrorType.Conflict);
+        public static Error UnAuthorized(string code = "General. UnAuthorized", string description = "General. UnAuthorized Error has occured") => new Error(code, description, ErrorType.UnAuthorized);
+        public static Error Forbidden(string code = "General.Forbidden", string description = "General.ForbiddenError has occured") => new(code, description, ErrorType.Forbidden);
+        public static Error InvalidCredential(string code = "General. InvalidCredential", string description = "General. InvalidCredential Error has occured") => new(code, description, ErrorType.InvalidCredential);
+
+
+
+
+    }
+
+    public enum ErrorType
+    {
+        Failure = 0 ,
+        Validation = 1,
+        NotFound = 2,
+        Conflict = 3,
+        UnAuthorized = 4,
+        Forbidden = 5,
+        InvalidCredential = 6 
+
+    }
+}
---
+++ E_commerce.Application/Common/Result.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Application.Common
+{
+    public class Result
+    {
+        public bool IsSucesss { get;  }
+        public IReadOnlyList< Error> Errors { get; }
+      protected Result(bool isSucess , IReadOnlyList<Error> erors)
+        {
+            IsSucesss = isSucess;
+            Errors = erors;
+        }
+
+
+        public static Result Ok() => new Result(true, Array.Empty<Error>());
+        public static Result Fail(Error error) => new(false,new [] { error });
+        public static Result Fail(IReadOnlyList<Error> errors) => new(false, errors);
+
+
+    }
+
+   public class Result<TValue> : Result
+    {
+        private readonly TValue _value;
+        public TValue data => IsSucesss ? _value : throw new InvalidOperationException("Cannot Access value of Fail Result");
+        private Result(TValue value) : base(true , Array.Empty<Error>())
+        {
+            _value = value;
+        }
+
+        private Result(Error error ) : base (false , new[] {error})
+        {
+            _value = default!;
+        }
+
+        private Result(IReadOnlyList<Error> errors) : base(false , errors)
+        {
+            _value = default!;
+        }
+        public static Result<TValue> Ok(TValue value) => new Result<TValue>(value);
+        public static Result<TValue> Fail(Error error) => new Result<TValue>(error);
+        public static Result<TValue> Fail(IReadOnlyList<Error> errors) => new Result<TValue>(errors);
+
+
+
+    }
+}
