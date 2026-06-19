127eef62506bde4f817adf00c9893006357bb410
jasmine <jasminemohamed174005@gmail.com>
2026-06-19 13:33:07 +0300

    start at Product module

---
+++ E_commerce.Application/Contracts/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using E_commerce.Application.Common;
+using E_commerce.Application.DTOs.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Application.Contracts
+{
+    public interface IProductService
+    {
+        public Task<Result<IReadOnlyList<ProductDto>>> GetAllProductsAsync(CancellationToken ct = default);
+        public Task<Result<ProductDto> ?> GetProductByIdAsync(int id , CancellationToken ct = default);
+        public Task<Result<IEnumerable<BrandDto>>> GetAllBrandAsync(CancellationToken ct = default);
+        public Task<Result<IEnumerable<TypeDto>>> GetAllTypesAsync(CancellationToken ct = default);
+    }
+}
